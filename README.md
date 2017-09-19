e6467f242f754e5ada9112b8cb40d804b88680d3
Michael Griggs <michael.griggs@gridgain.com>
2017-09-19 14:31:02 +0100

    Update README.md.  Change parameter name

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-# evictionservice
+# Eviction Service
 A sample of how to implement an external eviction service.  
 
 ## ServerNode
 Starts a server node with two example caches, DeclarationMaster and Declarations.  
 There is a one-to-many relationship between DeclarationMaster and Declarations.  
 
-# EvictionClient
+The ServerNode either loads data from Ignite Persistent Store, or clears the old data and creates new example data
+using a simple random algorithm.  Each declaration ID in DeclarationMaster will have between 500 and 1000 individual 
+rows in the Declarations cache.
+
+Usage: `ServerNode [--clear]`
+
+### Arguments  
+--clear: clears all previous data and re-creates with new example data. 
+
+
+## EvictionClient
 Starts a client node that takes an input parameter, rowNumberLimit.  If the number of rows in the Declarations cache exceeds rowNumberLimit, 
 EvictionClient will begin to delete declarations.  It does this using the following process:
 1.  `DeclarationMaster.remove(declarationId)`
 2.  Finds all the rowIds that relate to the declarationId, using a SQL query
 3.  Starts a DataStreamer on Declaration to remove all rows pertaining to the declarationId
 
-There is *no special logic* to determine which declaration is removed.  The next declaration is returned 
+There is **no special logic** to determine which declaration is removed.  The next declaration is returned 
 by the function `getDeclarationIdToEvict`.  This method should be re-implemented to give the behaviour desired.
 
 EvictionClient checks for excess rows once every 1000 milliseconds.  
--- servernode/src/main/java/com/gridgain/proserv/ServerNode.java
+++ servernode/src/main/java/com/gridgain/proserv/ServerNode.java
@@ -2,11 +2,6 @@ package com.gridgain.proserv;
 
 import com.gridgain.proserv.evictionservice.model.Declaration;
 import com.gridgain.proserv.evictionservice.model.DeclarationMaster;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Random;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
@@ -15,16 +10,18 @@ import org.apache.ignite.Ignition;
 
 public class ServerNode {
     public static void main(String[] args) {
-        boolean loadFromH2Database = false;
+        boolean createData = false;
 
-        // run with argument "--db" to create/populate a local H2 database
-        if (args.length > 0 && args[0].toLowerCase().equals("--db"))
-            loadFromH2Database = true;
+        // run with "--clear" argument to clear old data and generate new.  If no argument is supplied, previous data will be loaded from Ignite Persistent Store
+        if (args.length > 0 && args[0].toLowerCase().equals("--clear"))
+            createData = true;
+        else
+            System.out.printf("Loading data from Ignite Persistent Store...");
 
-        new ServerNode().run(loadFromH2Database);
+        new ServerNode().run(createData);
     }
 
-    private void run(boolean loadFromH2Database) {
+    private void run(boolean createData) {
 
         try (Ignite ignite = Ignition.start("ignite-server.xml")) {
 
@@ -35,14 +32,8 @@ public class ServerNode {
 
             IgniteCache<Integer, Declaration> declarationCache = ignite.getOrCreateCache("DeclarationCache");
 
-            try (Connection conn = getConnection()) {
-                createTables(conn);
-                if (loadFromH2Database)
-                    populateDatabaseAndCache(ignite, conn, masterCache, declarationCache);
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
+            if (createData)
+                createDataAndCache(ignite, masterCache, declarationCache);
 
             try {
                 while (true)
@@ -55,21 +46,13 @@ public class ServerNode {
         }
     }
 
-    private void populateDatabaseAndCache(Ignite ignite, Connection conn,
+    private void createDataAndCache(Ignite ignite,
         IgniteCache<Integer, DeclarationMaster> decMasterCache,
         IgniteCache<Integer, Declaration> decsCache) {
 
-        Random rng = new Random();
-        String declarationMasterSql = "INSERT INTO DECLARATION_MASTER(DECLARATION_ID) VALUES(%d)";
-        String declarationRowSql = "INSERT INTO DECLARATIONS(ROW_ID, DECLARATION_ID, VALUE) VALUES(%d,%d,%f)";
-
+        System.out.println("Generating data...");
         try (IgniteDataStreamer<Integer, Declaration> ds = ignite.dataStreamer(decsCache.getName())) {
             ds.allowOverwrite(true);
-            Statement stmt = conn.createStatement();
-
-            System.out.println("Truncate database tables");
-            stmt.execute("TRUNCATE TABLE DECLARATION_MASTER");
-            stmt.execute("TRUNCATE TABLE DECLARATIONS");
 
             System.out.println("Clear Ignite caches");
             decMasterCache.clear();
@@ -77,52 +60,20 @@ public class ServerNode {
 
             // populate database and cache independently, as customer does not use Ignite CacheStore implementation
             int decRow = 0;
+            Random rng = new Random();
+
             for (int decId = 0; decId < 100; decId++) {
-                stmt.execute(String.format(declarationMasterSql, decId));
                 decMasterCache.put(decId, new DeclarationMaster(decId));
 
-                int numberOfDeclarations = 500 + rng.nextInt(5000);
+                int numberOfDeclarations = 500 + rng.nextInt(500);
                 for (int j = 0; j < numberOfDeclarations; j++) {
                     float value = Math.abs(10_000 * rng.nextFloat());
                     ds.addData(decRow++, new Declaration(decId, decRow, value));
-                    stmt.execute(String.format(declarationRowSql, decRow, decId, value));
                 }
 
                 if (decId % 10 == 0)
                     System.out.println("Created and cached " + decId + " declarations");
             }
         }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Database loaded");
-    }
-
-    private void createTables(Connection conn) {
-        try {
-            PreparedStatement st = conn.prepareStatement("CREATE TABLE IF NOT EXISTS DECLARATION_MASTER(DECLARATION_ID int)"); // TRUNCATE TABLE DECLARATION_MASTER;");
-            st.execute();
-            st = conn.prepareStatement("CREATE TABLE IF NOT EXISTS DECLARATIONS(ROW_ID int, DECLARATION_ID int, VALUE float)");// TRUNCATE TABLE DECLARATIONS;");
-            st.execute();
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // start an H2 database server from the command line
-    private Connection getConnection() {
-        try {
-            Class.forName("org.h2.Driver");
-
-            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/EvictionExampleDatabase", "sa", "");
-            return conn;
-        }
-        catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
